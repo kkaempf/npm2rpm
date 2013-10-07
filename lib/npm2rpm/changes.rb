d43db80bb448d04dd09ea988a632cab19cfbd118
Klaus Kämpf <kkaempf@suse.de>
2013-10-07 12:52:40 +0200

    Drop HERE document in Changes

--- lib/npm2rpm/changes.rb
+++ lib/npm2rpm/changes.rb
@@ -1,22 +1,23 @@
-class Changes
-  def initialize name, metadata, options
-    @name = name
-    @metadata = metadata
-    @options = options
-  end
-  def write
-    # create .changes file as
-    # -------------------------------------------------------------------
-    # Sat Oct  5 12:29:03 UTC 2013 - kkaempf@suse.com
+module Npm2Rpm
 
-    File.open("#{@name}.changes", "w+") do |f|
-
-      f.puts <<EOF
--------------------------------------------------------------------
-#{@options.date} - #{@options.email}@suse.com
-
-- Initial version #{@metadata.version}
-EOF
+  class Changes
+    def initialize name, metadata, options
+      @name = name
+      @metadata = metadata
+      @options = options
+    end
+    def write
+      # create .changes file as
+      # -------------------------------------------------------------------
+      # Sat Oct  5 12:29:03 UTC 2013 - kkaempf@suse.com
+      
+      File.open("#{@name}.changes", "w+") do |f|
+        f.puts "-------------------------------------------------------------------"
+        f.puts "#{@options.date} - #{@options.email}@suse.com"
+        f.puts
+        f.puts "- Initial version #{@metadata.version}"
+      end
     end
   end
+
 end
