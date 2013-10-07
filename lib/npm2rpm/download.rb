1be21be2bf7f3d2a9507b1ec1994edb33f439b4a
Klaus Kämpf <kkaempf@suse.de>
2013-10-07 12:51:38 +0200

    Check server certificate

--- lib/npm2rpm/download.rb
+++ lib/npm2rpm/download.rb
@@ -1,22 +1,41 @@
-require 'openssl'
-require 'open-uri'
+#
+# npm2rpm/lib/download.rb
+#
+# Implements http(s) download
+#
 
-class Download
-  attr_reader :content
-  # get url to local file, return local file name
-  def initialize url
-    puts "Download #{url.inspect}"
-    @uri = URI.parse url
-#    http = Net::HTTP.new @uri
-#    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    @content = @uri.read :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE
-  end
-  def save
-    File.open(self.filename, "w+") { |f| f.write @content }
-    self
-  end
-  def filename
-    @filename ||= File.basename(@uri.path)
+require 'openssl'
+require 'net/https'
+require 'net/http'
+
+module Npm2Rpm
+  class Download
+    attr_reader :content
+    # get url to local file, return local file name
+    def initialize url
+      puts "Download #{url.inspect}"
+      @uri = URI url
+      http = Net::HTTP.new(@uri.host, @uri.port)
+      if @uri.scheme == "https"
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_PEER
+        
+        store = OpenSSL::X509::Store.new
+        store.set_default_paths # Optional method that will auto-include the system CAs.
+        crtpath = File.expand_path(File.join(File.dirname(__FILE__), "..", "registry.npmjs.org.crt"))                                           
+        store.add_file(crtpath)
+        http.cert_store = store
+      end
+      response = http.request(Net::HTTP::Get.new(@uri.request_uri))
+      raise "#{url} failed with #{response.code}" unless response.code.to_i == 200
+      @content = response.body
+    end
+    def save
+      File.open(self.filename, "w+") { |f| f.write @content }
+      self
+    end
+    def filename
+      @filename ||= File.basename(@uri.path)
+    end
   end
 end
-
