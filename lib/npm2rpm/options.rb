b088f8fa72bdc49fd531915b3108697233791cb6
Klaus Kämpf <kkaempf@suse.de>
2013-10-07 12:53:33 +0200

    options: restructure

--- lib/npm2rpm/options.rb
+++ lib/npm2rpm/options.rb
@@ -1,62 +1,67 @@
+#
+# npm2rpm/lib/options.rb
+#
+# Extract ARGV options
+#
+
 require 'getoptlong'
 
-class Options
-  attr_reader :package, :version, :date, :email, :user
+module Npm2Rpm
+  class Options
+    attr_reader :package, :version, :date, :email, :user
 
-  def initialize
+    def initialize
+      # .changes format
+      @date = `date -u +'%a %b %d %T UTC %Y'`
+      @date.chomp!
 
-    # .changes format
-    @date = `date -u +'%a %b %d %T UTC %Y'`
-    @date.chomp!
+      @email = ENV["USER"]
+      @user = `getent passwd $email`.split(':')[4]
+      @package = nil
 
-    @email = ENV["USER"]
-    @user = `getent passwd $email`.split(':')[4]
-    @package = nil
-
-    options = Options.new
-    # Parse command line options
-    GetoptLong.new(
-      [ '-h', '--help', GetoptLong::NO_ARGUMENT ],
-      [ '-H', '--man', GetoptLong::NO_ARGUMENT ],
-      [ '--specdir', GetoptLong::REQUIRED_ARGUMENT ],
-      [ '--sourcedir', GetoptLong::REQUIRED_ARGUMENT ],
-      [ '--user', GetoptLong::REQUIRED_ARGUMENT ],
-      [ '--email', GetoptLong::REQUIRED_ARGUMENT ],
-      [ '--date', GetoptLong::REQUIRED_ARGUMENT ],
-      [ '--package', GetoptLong::REQUIRED_ARGUMENT ]
-    ).each do |opt, arg|
-      case opt
-      when '--specdir'
-        options.specdir = arg
-      when '--sourcedir'
-        options.sourcedir = arg
-      when '--user'
-        options.user = arg
-      when '--email'
-        options.email = arg
-      when '--date'
-        options.date = arg
-      when '--package'
-        options.package = arg
-      else
-        "Run $0 -h or $0 -H for details on usage";
+      # Parse command line options
+      GetoptLong.new(
+        [ '-h', '--help', GetoptLong::NO_ARGUMENT ],
+        [ '-H', '--man', GetoptLong::NO_ARGUMENT ],
+        [ '--specdir', GetoptLong::REQUIRED_ARGUMENT ],
+        [ '--sourcedir', GetoptLong::REQUIRED_ARGUMENT ],
+        [ '--user', GetoptLong::REQUIRED_ARGUMENT ],
+        [ '--email', GetoptLong::REQUIRED_ARGUMENT ],
+        [ '--date', GetoptLong::REQUIRED_ARGUMENT ],
+        [ '--package', GetoptLong::REQUIRED_ARGUMENT ]
+      ).each do |opt, arg|
+        case opt
+        when '--specdir'
+          @specdir = arg
+        when '--sourcedir'
+          @sourcedir = arg
+        when '--user'
+          @user = arg
+        when '--email'
+          @email = arg
+        when '--date'
+          @date = arg
+        when '--package'
+          self.package = arg
+        else
+          "Run $0 -h or $0 -H for details on usage";
+        end
       end
-    end
-
-    unless ARGV.empty?
-      if options.package
-        abort "Extra arguments: #{ARGV}"
+      unless ARGV.empty?
+        if @package
+          abort "Extra arguments: #{ARGV}"
+        end
+        self.package = ARGV.shift
       end
-      options.package = ARGV.shift
+
+      abort 'Missing package argument' unless @package;
+      abort "Extra arguments: #{ARGV}" unless ARGV.empty?;
+
     end
-
-    abort 'Missing package argument' unless options.package;
-    abort "Extra arguments: #{ARGV}" unless ARGV.empty?;
-
-  end
   
-  def package= name
-    @package, @version = name.split "@"
+    def package= name
+      @package, @version = name.split "@"
+    end
   end
-end
 
+end
